429ccd880bd7d841362bdc8965723bb3893d02ae
Bikash Sah <51731962+BikashSah999@users.noreply.github.com>
2023-12-01 16:04:38 +0545

    chore: ++

--- src/lib/serializeMdxSource.ts
+++ src/lib/serializeMdxSource.ts
@@ -1,8 +1,8 @@
 import rehypePrism from '@mapbox/rehype-prism'
 import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeSlug from 'rehype-slug'
+// import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+// import rehypeSlug from 'rehype-slug'
 // import gfm from 'remark-gfm'
 
 export const serializeMdxSource = async (markdownContent: string): Promise<MDXRemoteSerializeResult> => {
@@ -13,21 +13,21 @@ export const serializeMdxSource = async (markdownContent: string): Promise<MDXRe
                 // Adds syntax highlighting
                 rehypePrism,
 
-                // Adds "slug" IDs to each heading, for links and table of contents.
-                rehypeSlug,
+                // // Adds "slug" IDs to each heading, for links and table of contents.
+                // rehypeSlug,
 
-                // Adds auto links to each heading.
-                [
-                    rehypeAutolinkHeadings,
-                    {
-                        properties: {
-                            className: 'anchor',
-                            ariaHidden: true,
-                            tabIndex: -1,
-                        },
-                        behavior: 'prepend',
-                    },
-                ],
+                // // Adds auto links to each heading.
+                // [
+                //     rehypeAutolinkHeadings,
+                //     {
+                //         properties: {
+                //             className: 'anchor',
+                //             ariaHidden: true,
+                //             tabIndex: -1,
+                //         },
+                //         behavior: 'prepend',
+                //     },
+                // ],
             ],
         },
     })
