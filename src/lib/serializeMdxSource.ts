3bd5b76b54b6b59e9a69068f10e57286159b9780
Bikash Sah <51731962+BikashSah999@users.noreply.github.com>
2023-12-04 12:50:35 +0545

    chore: revert

--- src/lib/serializeMdxSource.ts
+++ src/lib/serializeMdxSource.ts
@@ -1,33 +1,33 @@
 import rehypePrism from '@mapbox/rehype-prism'
 import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
-// import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-// import rehypeSlug from 'rehype-slug'
-// import gfm from 'remark-gfm'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
+import gfm from 'remark-gfm'
 
 export const serializeMdxSource = async (markdownContent: string): Promise<MDXRemoteSerializeResult> => {
     const serializeResult = await serialize(markdownContent, {
         mdxOptions: {
-            // remarkPlugins: [gfm],
+            remarkPlugins: [gfm],
             rehypePlugins: [
                 // Adds syntax highlighting
                 rehypePrism,
 
-                // // Adds "slug" IDs to each heading, for links and table of contents.
-                // rehypeSlug,
+                // Adds "slug" IDs to each heading, for links and table of contents.
+                rehypeSlug,
 
-                // // Adds auto links to each heading.
-                // [
-                //     rehypeAutolinkHeadings,
-                //     {
-                //         properties: {
-                //             className: 'anchor',
-                //             ariaHidden: true,
-                //             tabIndex: -1,
-                //         },
-                //         behavior: 'prepend',
-                //     },
-                // ],
+                // Adds auto links to each heading.
+                [
+                    rehypeAutolinkHeadings,
+                    {
+                        properties: {
+                            className: 'anchor',
+                            ariaHidden: true,
+                            tabIndex: -1,
+                        },
+                        behavior: 'prepend',
+                    },
+                ],
             ],
         },
     })
