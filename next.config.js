758baa49c2ad3b28bc5e2ecdf214685c042aa7a8
Bikash Sah <51731962+BikashSah999@users.noreply.github.com>
2023-12-04 16:43:02 +0545

    chore: +++++

--- next.config.js
+++ next.config.js
@@ -36,4 +36,15 @@ const nextConfig = {
   },
 }
 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
+
 module.exports = withBundleAnalyzer(nextConfig)
